9132afa5c9a2e5723af2c08c96d9fe6f1e0491bf
David Della Rossa <david.dellarossa@gmail.com>
2022-04-04 08:54:51 +0200

    Scribble added with animation

---
+++ Assets/Scripts/Enemies/Scribble/StateMachine/IdleState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletHellJam2022.Assets.Scripts.Enemies.Scribble.StateMachine
+{
+    public class IdleState : ScribbleState
+    {
+        public override event Action<IScribbleState> ChangeState;
+
+        public IdleState(ScribbleControllerCore parent, StateFactory factory)
+        {
+            Parent = parent;
+            Factory = factory;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/Scribble/StateMachine/StateFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletHellJam2022.Assets.Scripts.Enemies.Scribble.StateMachine
+{
+    public class StateFactory
+    {
+        public ScribbleControllerCore Parent { get; set; }
+
+        public IScribbleState AttackState { get; protected set; }
+
+        public IScribbleState SeekState { get; protected set; }
+
+        public IScribbleState IdleState { get; protected set; }
+
+        public StateFactory(ScribbleControllerCore parent)
+        {
+            this.Parent = parent;
+            AttackState = new AttackState(this.Parent, this);
+            SeekState = new SeekState(this.Parent, this);
+            IdleState = new IdleState(this.Parent, this);
+        }
+    }
+}
